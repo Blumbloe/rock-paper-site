99822dfc42c0b1457bade6543138a721f10c7473
Blumbloe <c05w@icloud.com>
2025-12-07 17:02:30 +0000

    Add new game function and query selector for all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
+/* Help button */
 $(document).ready(function(){
     $("button#help").click(function(){
         $("#hide").toggle();
     });
 });
+
+/* Game */
+
+function newGame() {
+    game.cScore = 0;
+}
+
+
+
+const elements = document.querySelectorAll(".gameBtn");
+elements.forEach(button =>  {
+    button.addEventListener("click", (e) => {
+       let buttonOutcome = e.target.getAttribute("data-button-type")
+
+      
+       
+        console.log(result(buttonOutcome))
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
