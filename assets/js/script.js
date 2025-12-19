8ec66f1a73ec688aece8c3ccc8525d652bc3b3d8
Blumbloe <c05w@icloud.com>
2025-12-19 14:56:56 +0000

    Change help button to a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
 
 highScore()
 
+/* Modal */
+var modal = document.getElementById("Modal");
+
+var btn = document.getElementById("modalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+ 
 /* Game */
 
 function newGame() {
