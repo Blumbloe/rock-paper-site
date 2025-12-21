d02f85ee7200de23f9bb21e025b80391e6e1e57f
Blumbloe <c05w@icloud.com>
2025-12-21 20:16:56 +0000

    Use Prettier - Code Formater extension to clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-
-highScore()
+highScore();
 
 /* Modal */
 var modal = document.getElementById("Modal");
@@ -8,139 +7,120 @@ var btn = document.getElementById("modalBtn");
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
- 
 /* Game */
 
 function newGame() {
-   document.getElementById("current-score").innerText = 0;
+  document.getElementById("current-score").innerText = 0;
 }
 
 function resetGame() {
-   document.getElementById("current-score").innerText = 0;
-   document.getElementById("Outcome").innerText = "";
-   document.getElementById("youchose").innerText = "";
-   document.getElementById("computerchose").innerText = "";
+  document.getElementById("current-score").innerText = 0;
+  document.getElementById("Outcome").innerText = "";
+  document.getElementById("youchose").innerText = "";
+  document.getElementById("computerchose").innerText = "";
 }
-
 
 const elements = document.querySelectorAll(".gameBtn");
-elements.forEach(button =>  {
-    button.addEventListener("click", (e) => {
-       let buttonOutcome = e.target.getAttribute("data-button-type")
+elements.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let buttonOutcome = e.target.getAttribute("data-button-type");
 
-        console.log(result(buttonOutcome))
-           document.getElementById("youchose").innerText = buttonOutcome
-    
-    });
+    console.log(result(buttonOutcome));
+    document.getElementById("youchose").innerText = buttonOutcome;
+  });
 });
 
+function computerValue() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
 
-
-function computerValue(){
-    const randNum = Math.floor(Math.random() * 3) + 1;
-    
-    switch(randNum){
-        case 1:
-            computer = "rock";
-            break;
-        case 2:
-            computer = "paper";
-            break;
-        case 3:
-            computer = "scissors";
-            break;
-    }
-    document.getElementById("computerchose").innerText = computer
-    return computer;
+  switch (randNum) {
+    case 1:
+      computer = "rock";
+      break;
+    case 2:
+      computer = "paper";
+      break;
+    case 3:
+      computer = "scissors";
+      break;
+  }
+  document.getElementById("computerchose").innerText = computer;
+  return computer;
 }
 
-function result(buttonValue){
-    let computedValue = computerValue()
-    let playerValue = buttonValue
-    let gameResult = ""
+function result(buttonValue) {
+  let computedValue = computerValue();
+  let playerValue = buttonValue;
+  let gameResult = "";
 
-    if(playerValue == computedValue){
-        gameResult = "Draw!";
+  if (playerValue == computedValue) {
+    gameResult = "Draw!";
+  } else if (computedValue == "rock") {
+    if (playerValue == "paper") {
+      gameResult = "You Win!";
+      incrementScore();
+      highScore();
+    } else {
+      gameResult = "You Lose!";
+      newGame();
     }
-    else if(computedValue == "rock"){ 
-       if (playerValue == "paper"){
-            gameResult = "You Win!"
-            incrementScore()
-            highScore()
-       } else {
-            gameResult = "You Lose!"
-            newGame()
-       }
+  } else if (computedValue == "paper") {
+    if (playerValue == "scissors") {
+      gameResult = "You Win!";
+      incrementScore();
+      highScore();
+    } else {
+      gameResult = "You Lose!";
+      newGame();
     }
-    else if(computedValue == "paper"){ 
-       if (playerValue == "scissors"){
-            gameResult = "You Win!"
-            incrementScore()
-            highScore()
-       } else {
-            gameResult = "You Lose!"
-            newGame()
-       }
+  } else if (computedValue == "scissors") {
+    if (playerValue == "rock") {
+      gameResult = "You Win!";
+      incrementScore();
+      highScore();
+    } else {
+      gameResult = "You Lose!";
+      newGame();
     }
-    else if(computedValue == "scissors"){ 
-       if (playerValue == "rock"){
-            gameResult = "You Win!"
-            incrementScore()
-            highScore()
-       } else {
-            gameResult = "You Lose!"
-            newGame()
-       }
-    }
-    const matchOutcome = document.querySelector("#Outcome");
-    matchOutcome.innerText = gameResult 
- 
-} 
+  }
+  const matchOutcome = document.querySelector("#Outcome");
+  matchOutcome.innerText = gameResult;
+}
 
 function incrementScore() {
-    let currentScore = parseInt(document.getElementById("current-score").innerText);
-    ++currentScore
-    document.getElementById("current-score").innerText = currentScore;
+  let currentScore = parseInt(
+    document.getElementById("current-score").innerText
+  );
+  ++currentScore;
+  document.getElementById("current-score").innerText = currentScore;
 }
 
 function highScore() {
-   let currentScore = (document.getElementById("current-score").innerText);
-   let highScore = localStorage.getItem("hs")
-   if (!highScore){
-     localStorage.setItem("hs", 0)
-   } else {
-      document.getElementById("high-score").innerText = highScore
-   }
+  let currentScore = document.getElementById("current-score").innerText;
+  let highScore = localStorage.getItem("hs");
+  if (!highScore) {
+    localStorage.setItem("hs", 0);
+  } else {
+    document.getElementById("high-score").innerText = highScore;
+  }
 
-    
-    if (highScore < currentScore) {
-        localStorage.setItem("hs", currentScore)
-        newScore = localStorage.getItem("hs")
-        document.getElementById("high-score").innerText = newScore
-    }
-
+  if (highScore < currentScore) {
+    localStorage.setItem("hs", currentScore);
+    newScore = localStorage.getItem("hs");
+    document.getElementById("high-score").innerText = newScore;
+  }
 }
-
-
-
-
-
-
-
-
-
-
